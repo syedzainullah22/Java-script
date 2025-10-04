9841f12c5067aaa08df832e4ef26ca144d1ee319
syedzainullah22 <syedzainullah229@gmail.com>
2025-10-04 15:45:42 +0530

    Brief on javaScrit

---
+++ INTRODUCTIO TO html dom/script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    var a =4;
+    document.getElememtById("demo").innerHTML = a*a;
+}
---
+++ INTRODUCTIO TO html dom/validation.js
@@ -0,0 +1,8 @@
+function validate(e) {
+    e.preventDefault():
+    const email = document.getElementById('email').value;
+     const pass = document.getElementById('password').value;
+      const age = document.getElementById('age').value;
+       const msgbox= document.getElementById('message').value;
+       let message = '';
+       if(email === '') {}
